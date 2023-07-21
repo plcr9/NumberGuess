903979e874417666a76696856747958221eba029
Paul <118978940+plcr9@users.noreply.github.com>
2023-07-21 15:58:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from random import randint
+fav_number = 34
+guess_correct = False
 
+while not guess_correct:
+  guess = randint(0, 100)
+  if guess == fav_number:
+    print("You guessed right: 34!")
+    guess_correct = True
+  else:
+    print(f"{guess} is wrong! Try again!")
